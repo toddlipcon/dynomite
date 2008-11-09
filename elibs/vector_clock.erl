7d2e41eed06aa3b4eb457a908ed6c29203a21cdc
Todd Lipcon <todd@amiestreet.com>
2008-11-09 14:18:45 -0500

    Rewrite vector_clock to use orddict - about 4x speed improvement on compare

--- elibs/vector_clock.erl
+++ elibs/vector_clock.erl
@@ -2,20 +2,15 @@
 -export ([create/1, increment/2, compare/2, resolve/2, merge/2]).
 
 -ifdef(TEST).
+-compile(export_all).
 -include("etest/vector_clock_test.erl").
 -endif.
 
 create(NodeName) -> [{NodeName, 1}].
 
-increment(NodeName, [{NodeName, Version}|Clocks]) ->
-	[{NodeName, Version+1}|Clocks];
+increment(NodeName, Clock) ->
+  orddict:update_counter(NodeName, 1, Clock).
 
-increment(NodeName, [NodeClock|Clocks]) ->
-	[NodeClock|increment(NodeName, Clocks)];
-	
-increment(NodeName, []) ->
-	[{NodeName, 1}].
-	
 resolve({ClockA, ValuesA}, {ClockB, ValuesB}) ->
   case compare(ClockA, ClockB) of
     less -> {ClockB, ValuesB};
@@ -25,62 +20,63 @@ resolve({ClockA, ValuesA}, {ClockB, ValuesB}) ->
   end.
   
 merge(ClockA, ClockB) ->
-  merge([], ClockA, ClockB).
+  PickGreater = fun
+    (_Key, AVal, BVal) when AVal > BVal -> AVal;
+    (_Key, AVal, BVal) when BVal >= AVal -> BVal
+  end,
+  orddict:merge(PickGreater, ClockA, ClockB).
 
-merge(Merged, [], ClockB) -> lists:keysort(1, Merged ++ ClockB);
 
-merge(Merged, ClockA, []) -> lists:keysort(1, Merged ++ ClockA);
-  
-merge(Merged, [{NodeA, VersionA}|ClockA], ClockB) ->
-  case lists:keytake(NodeA, 1, ClockB) of
-    {value, {NodeA, VersionB}, TrunkClockB} when VersionA > VersionB ->
-      merge([{NodeA,VersionA}|Merged],ClockA,TrunkClockB);
-    {value, {NodeA, VersionB}, TrunkClockB} ->
-      merge([{NodeA,VersionB}|Merged],ClockA,TrunkClockB);
-    false ->
-      merge([{NodeA,VersionA}|Merged],ClockA,ClockB)
-  end.
-	
+% Internal for the comparison fold to make things cleaner
+-record(cmp_state, {seen_a_gt = false,
+                    seen_eq = false,
+                    seen_b_gt = false}).
+
 compare(ClockA, ClockB) ->
-  AltB = less_than(ClockA, ClockB),
-  BltA = less_than(ClockB, ClockA),
-  AeqB = equals(ClockA, ClockB),
-  AccB = concurrent(ClockA, ClockB),
-  if
-    AltB -> less;
-    BltA -> greater;
-    AeqB -> equal;
-    AccB -> concurrent
+  CS = int_compare(ClockA, ClockB, #cmp_state{}),
+  case CS of
+    #cmp_state{seen_a_gt = true,
+               seen_b_gt = false} -> greater;
+    #cmp_state{seen_a_gt = false,
+               seen_b_gt = true} -> less;
+    #cmp_state{seen_a_gt = true,
+               seen_b_gt = true} -> concurrent;
+    #cmp_state{seen_a_gt = false,
+               seen_b_gt = false,
+               seen_eq = true} -> equal
   end.
-	
-% ClockA is less than ClockB if and only if ClockA[z] <= ClockB[z] for all instances z and there
-% exists an index z' such that ClockA[z'] < ClockB[z']
-less_than(ClockA, ClockB) ->
-  ForAll = lists:all(fun({Node, VersionA}) ->
-    case lists:keysearch(Node, 1, ClockB) of
-      {value, {_NodeB, VersionB}} -> VersionA =< VersionB;
-      false -> false
-    end
-  end, ClockA),
-  Exists = lists:any(fun({NodeA, VersionA}) ->
-    case lists:keysearch(NodeA, 1, ClockB) of
-      {value, {_NodeB, VersionB}} -> VersionA /= VersionB;
-      false -> true
-    end
-  end, ClockA),
-  %length takes care of the case when clockA is shorter than B
-  ForAll and (Exists or (length(ClockA) < length(ClockB))).
-  
-equals(ClockA, ClockB) ->
-  Equivalent = lists:all(fun({NodeA, VersionA}) ->
-    lists:any(fun(NodeClockB) ->
-      case NodeClockB of
-        {NodeA, VersionA} -> true;
-        _ -> false
-      end
-    end, ClockB)
-  end, ClockA),
-  Equivalent and (length(ClockA) == length(ClockB)).
-  
-concurrent(ClockA, ClockB) ->
-  not (less_than(ClockA, ClockB) or less_than(ClockB, ClockA) or equals(ClockA, ClockB)).
+
+%% A shared node
+int_compare([{Node, ValA} | RestA],
+            [{Node, ValB} | RestB],
+            CState) ->
+  NewCS = if
+    ValA > ValB   -> CState#cmp_state{seen_a_gt = true};
+    ValA =:= ValB -> CState#cmp_state{seen_eq = true};
+    ValB > ValA   -> CState#cmp_state{seen_b_gt = true}
+  end,
+  int_compare(RestA, RestB, NewCS);
+
+% A node one has but the other doesn't
+int_compare(A = [{NodeA, ValA} | RestA],
+            B = [{NodeB, ValB} | RestB],
+            CState) ->
+  if
+    NodeA < NodeB ->
+      int_compare(RestA, B, CState#cmp_state{seen_a_gt = true});
+    NodeB < NodeA ->
+      int_compare(A, RestB, CState#cmp_state{seen_b_gt = true})
+  end;
+
+% A has more elements
+int_compare(A = [Hd | Rest],
+            B = [], CState) ->
+  CState#cmp_state{seen_a_gt = true};
+
+% B has more elements
+int_compare(A = [],
+            B = [Hd | Rest], CState) ->
+  CState#cmp_state{seen_b_gt = true};
+
+% Out of elements
+int_compare([], [], CState) -> CState.
--- etest/vector_clock_test.erl
+++ etest/vector_clock_test.erl
@@ -13,46 +13,24 @@ increment_clock_test() ->
   Clock6 = increment(b, Clock5),
   [{a, 2}, {b, 3}, {c, 1}] = Clock6.
   
-less_than_concurrent_test() ->
+concurrent_test() ->
   ClockA = [{b, 1}],
   ClockB = [{a, 1}],
-  false = less_than(ClockA, ClockB),
-  false = less_than(ClockB, ClockA).
+  concurrent = compare(ClockA, ClockB),
+  concurrent = compare(ClockB, ClockA).
 
 less_than_causal_test() ->
   ClockA = [{a,2}, {b,4}, {c,1}],
   ClockB = [{c,1}],
-  true = less_than(ClockB, ClockA),
-  false = less_than(ClockA, ClockB).
-  
+  less = compare(ClockB, ClockA),
+  greater = compare(ClockA, ClockB).
+ 
 less_than_causal_2_test() ->
   ClockA = [{a,2}, {b,4}, {c,1}],
   ClockB = [{a,3}, {b,4}, {c,1}],
-  true = less_than(ClockA, ClockB),
-  false = less_than(ClockB, ClockA).
-  
-mixed_up_ordering_test() ->
-  ClockA = [{b,4}, {a,2}],
-  ClockB = [{a,1}, {b,3}],
-  true = less_than(ClockB, ClockA),
-  false = less_than(ClockA, ClockB).
-  
-equivalence_test() ->
-  ClockA = [{b,4}, {a,2}],
-  ClockB = [{a,2}, {b,4}],
-  false = less_than(ClockA, ClockA),
-  false = less_than(ClockA, ClockB),
-  false = less_than(ClockB, ClockA),
-  true = equals(ClockA, ClockA),
-  true = equals(ClockB, ClockA),
-  true = equals(ClockA, ClockB),
-  false = equals(ClockA, [{a,3}, {b,3}]).
-  
-concurrent_test() ->
-  ClockA = [{a,1}],
-  ClockB = [{b,1}],
-  true = concurrent(ClockA, ClockB).
-  
+  less = compare(ClockA, ClockB),
+  greater = compare(ClockB, ClockA).
+ 
 simple_merge_test() ->
   ClockA = [{a,1}],
   ClockB = [{b,1}],
@@ -67,3 +45,15 @@ overlap_unequal_merge_test() ->
   ClockA = [{a,3},{b,4}],
   ClockB = [{a,4},{c,5}],
   [{a,4},{b,4},{c,5}] = merge(ClockA, ClockB).
+
+compare_test() ->
+  Tests = [
+    {[{a, 2}], [{a, 1}], greater},
+    {[{a, 2}], [{a, 2}], equal},
+    {[{a, 2}], [], greater},
+    {[],       [{a, 2}], less},
+    {[{a, 2}], [{b, 2}], concurrent},
+    {[{a, 2}, {b, 3}], [{b, 2}], greater},
+    {[{a, 2}, {b, 1}], [{b, 2}], concurrent}
+    ],
+  [{A,B,Res} = {A,B,compare(A, B)} || {A, B, Res} <- Tests].
